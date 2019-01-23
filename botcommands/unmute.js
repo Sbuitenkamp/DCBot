ef9980f94c59be3fb6b8152391813d5d81150a1f
Steven <buitenkamp.developer@gmail.com>
2019-01-23 11:40:36 +0100

    repurpose

--- botcommands/unmute.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'unmute',
-    execute(message, muteHandler) {
-        muteHandler(message, message.member, false);
-        message.member.send(`You have been unmuted.`).catch(e => console.log(`${message.member.displayName} doesn't accept dms`));
-    }
-};
--- botcommands/warn.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'warn',
-    async execute(person, warningHandler, reason) {
-        warningHandler(person, reason)
-    }
-};
